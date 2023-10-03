786735003092bf78095c714774a024c1c06d6c54
Keaton Singer <keatonsinger@gmail.com>
2023-10-03 10:28:00 -0700

    Created concurrent command for starting client and server

---
+++ proxi-poll/config-overrides.js
@@ -0,0 +1,5 @@
+const paths = require('react-scripts/config/paths')
+const path = require("path");
+
+paths.appSrc = path.resolve(__dirname, 'client')
+paths.appIndexJs = path.resolve(__dirname, 'client/index.js')
