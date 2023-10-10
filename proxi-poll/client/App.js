c038ca612107445d48408cedd348026571b26195
Keaton Singer <keatonsinger@gmail.com>
2023-10-10 10:21:00 -0700

    Prototyping search page

--- proxi-poll/client/App.js
+++ proxi-poll/client/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import PollSearch from "./PollSearch/PollSearch.js"
 import MenuIcon from '@mui/icons-material/Menu';
 import "./App.css";
 
@@ -9,19 +10,22 @@ function App() {
     fetch("http://127.0.0.1:7500/test")
       .then((res) => res.json())
       .then((data) => setTestData(data["title"]))
-      .catch((err) => console.log("Error fetching test data"));
+      .catch((err) => {setTestData("ProxiPoll"); console.log("Error fetching test data")});
   }, []);
 
   return (
     <div>
+      <PollSearch></PollSearch>
+      <div>
         <header className="App-header">
           <MenuIcon className="App-header-menu-icon"/>
-          <div className="App-header-title">
-            {testData}
+            <div className="App-header-title">
+              {testData}
+            </div>
+          </header>
+          <div className="Main-page">
           </div>
-        </header>
-        <div className="Main-page">
-        </div>
+      </div>
     </div>
   );
 }
