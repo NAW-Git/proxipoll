d6902c631389dc9eefa85516a813cef1adea96c7
Keaton Singer <keatonsinger@gmail.com>
2023-10-17 20:36:44 -0700

    Please don't break dev

--- proxi-poll/config-overrides.js
+++
@@ -1,5 +0,0 @@
-const paths = require('react-scripts/config/paths')
-const path = require("path");
-
-paths.appSrc = path.resolve(__dirname, 'client')
-paths.appIndexJs = path.resolve(__dirname, 'client/index.js')
