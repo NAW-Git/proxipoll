29e0c6b33d4451b89abf2aa00140002691626e85
Keaton Singer <keatonsinger@gmail.com>
2023-10-23 18:26:58 -0700

    Added Back 'Replace Me!'

--- proxipoll/frontend/src/PollSearch/PollSearch.tsx
+++ proxipoll/frontend/src/PollSearch/PollSearch.tsx
@@ -1,7 +1,7 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Map from './Map.png'
 import "./PollSearch.css";
+import Map from './Map.png'
 
 function PollSearch() {
 
